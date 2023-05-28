41413a20e75190f47da17e0caadf29fd88e76aad
cian66 <65384234@qq.com>
2023-05-28 21:25:43 +0800

    1.0.1-删除idea中的文件

--- src/main/java/cn/cian/test/HelloWord.java
+++ src/main/java/cn/cian/test/HelloWord.java
@@ -3,6 +3,6 @@ package cn.cian.test;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello word");
-        System.out.println("修改过后");
+        System.out.println("如果删除了在修改呢");
     }
 }
