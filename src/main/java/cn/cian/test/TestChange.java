a6bdb3405e25328eebe5bac9d886ff6ddd6048a7
cian66 <65384234@qq.com>
2023-07-16 21:48:22 +0800

    添加循环

--- src/main/java/cn/cian/test/TestChange.java
+++ src/main/java/cn/cian/test/TestChange.java
@@ -2,6 +2,8 @@ package cn.cian.test;
 
 public class TestChange {
     public static void main(String[] args) {
-        System.out.println("我再次提交");
+        for (int i = 0; i < 5; i++) {
+            
+        }
     }
 }
